aee5a0b94935fabae61f4f77f35c6b5caa95b361
themissingxerson <goswamiaditya758@gmail.com>
2025-12-27 21:37:42 +0530

    Implement basic chatbot interaction loop

---
+++ chatbot/chatbot.py
@@ -0,0 +1,10 @@
+while True:
+    a = input("You: ")
+
+    if a == "hi":
+        print("Bot: hello")
+    elif a == "bye":
+        print("Bot: goodbye")
+        break
+    else:
+        print("Bot: thank you")
